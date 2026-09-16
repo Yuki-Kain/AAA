d869a1f88d12a692d8fb39ea7de04af40e18e4cc
Yuki-Kain <ryanlimaabatista@gmail.com>
2026-09-16 11:25:25 -0300

    Add Contador.java to count from 1 to 100

---
+++ Atividade/Contador.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main (String [] args){
+
+    
+    int contador; // declaração do contador
+    contador = 1; // inicializa o contador
+
+    while (contador <=100){
+        System.out.println(contador + " ");
+        contador++;
+
+}   
+}
+}
