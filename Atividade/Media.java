ce7c4dab361733999a50517fb2c21b0b6f846949
Yuki-Kain <ryanlimaabatista@gmail.com>
2026-09-23 08:40:38 -0300

    Media.java
    Fix syntax errors and implement note input loop.

---
+++ Atividade/Media.java
@@ -0,0 +1,17 @@
+import jaca.util.Scanner;
+
+public class Media {
+  public static void main (Strings [] args);{
+ 
+  Scanner entrada - new Scanner (System.in);
+
+    int contador = 0;
+    double nota, media, acumuladorNotas;
+
+    while (contador < 5){
+      contador ++;
+    System.out.println("Digite uma nota" + contador);
+    nota =  entrada.nextDouble();
+    }        
+  }
+}
